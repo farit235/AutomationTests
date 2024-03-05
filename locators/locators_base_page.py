774d728dbb239650eb572b39b07f1dece9522287
Farit Sibgatulin <farit@zelax.ru>
2024-03-05 13:49:34 +0300

    changed new device button xpath

--- locators/locators_base_page.py
+++ locators/locators_base_page.py
@@ -1,8 +1,9 @@
 from selenium.webdriver.common.by import By
 
+
 class BasePageLocators:
     """Constant locators for elements on Base page"""
-    NEW_DEVICE_ITEM_BUTTON = (By.XPATH, "//button[text()='New device']")
+    NEW_DEVICE_ITEM_BUTTON = (By.XPATH, "//button[@class='btn btn-primary']")
     INPUT_DEVICE_NAME_FIELD = (By.XPATH, "//textarea[@id='device-upsert-modal__device-name-field']")
     INPUT_DEVICE_ID_FIELD = (By.XPATH, "//textarea[@id='device-upsert-modal__device-id-field']")
     SUBMIT_NEW_DEVICE_BUTTON = (By.XPATH, "//button[@type='button'][contains(.,'Submit')]")
